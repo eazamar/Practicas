923c9f565a121fbd732350781d8717f42416b58e
Emmanuel Azamar <emmanuel.azamar@eximagen.com.mx>
2025-04-30 12:05:42 -0600

    Ciclo for

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+
+        final String saludoConst = "Hello and welcome!";
+        int[] listaNum = new int[]{5,4,3,2,1};
+
+        System.out.println(saludoConst);
+
+        for(var x: listaNum){
+            System.out.println("i = " + x);
+        }
+
+        System.out.println("****************");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
